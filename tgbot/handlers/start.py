87103b17796e55cce8a4a16bdb52aa317c694665
chekunov <v.chekunov@media108.ru>
2022-09-14 11:38:01 +0300

    #6270810 Логирование статистики регистрации в боте.

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -22,22 +22,24 @@ async def start_deep_link(message: Message):
             site_id = values.split('&')[2].split('=')[1]
             campaign_id = values.split('&')[3].split('=')[1]
             await create_userbot(message, building_name, calltracking, s_id=site_id, c_id=campaign_id)
+            await log_stat(message.from_user, event='Регистрация в боте')
         else:
             building_name = values.split('&')[0]
             calltracking = values.split('&')[1].split('=')[1]
             source = values.split('&')[2].split('=')[0]
             source_id = values.split('&')[2].split('=')[1]
             await create_userbot(message, building_name, calltracking, **{source: source_id})
+            await log_stat(message.from_user, event='Регистрация в боте')
     else:
         campaign = await get_start_campaign()
         building_name = campaign.building.latin_name
         calltracking = campaign.call_tracking_name
         source_id = campaign.get_source_id
         await create_userbot(message, building_name, calltracking, **source_id)
+        await log_stat(message.from_user, event='Регистрация в боте')
     building = await get_building(building_name)
     markup = await main_building_menu(building_name)
     await message.answer(text=f'{building.greeting}', reply_markup=markup)
-    await log_stat(message.from_user, event='Регистрация в боте')
 
 
 async def start(message: Message):
