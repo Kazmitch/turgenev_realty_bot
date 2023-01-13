dd9429307f5f138cfcfd34b211f224a05730b065
chekunov <v.chekunov@media108.ru>
2023-01-13 19:34:15 +0300

    #6427141 Добавлена рассылка в бота.

---
+++ realty_bot/realty_bot/mailing.py
@@ -0,0 +1,54 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.utils import exceptions
+
+from tgbot.config import load_config
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(f"/home/vitamin/m108/realty_telegram_bot/logs/mailing.log")
+formatter = logging.Formatter("%(name)s - %(asctime)s - %(levelname)s - %(message)s", datefmt='%d.%m.%Y %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+config = load_config('.env')
+bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+dp = Dispatcher(bot)
+
+
+async def send_message(user_id: int, text: str, photo: str, disable_notification: bool = False) -> bool:
+    """Делаем отправку сообщения."""
+    try:
+        await bot.send_photo(user_id, photo, text, disable_notification=disable_notification)
+    except exceptions.BotBlocked:
+        logger.error(f"Target [ID:{user_id}]: blocked by user")
+    except exceptions.ChatNotFound:
+        logger.error(f"Target [ID:{user_id}]: invalid user ID")
+    except exceptions.RetryAfter as e:
+        logger.error(f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
+        await asyncio.sleep(e.timeout)
+        return await send_message(user_id, text, photo)  # Recursive call
+    except exceptions.UserDeactivated:
+        logger.error(f"Target [ID:{user_id}]: user is deactivated")
+    except exceptions.TelegramAPIError:
+        logger.exception(f"Target [ID:{user_id}]: failed")
+    else:
+        logger.info(f"Target [ID:{user_id}]: success")
+        return True
+    return False
+
+
+async def broadcaster(users_list: list, text: str, image: str) -> int:
+    """Выполняем рассылку."""
+    count = 0
+    try:
+        for user_id in users_list:
+            if await send_message(user_id, text, image):
+                count += 1
+            await asyncio.sleep(.05)  # 20 messages per second (Limit: 30 messages per second)
+    finally:
+        logger.info(f"{count} messages successful sent.")
+
+    return count
