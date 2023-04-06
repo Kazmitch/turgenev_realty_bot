2ca11915a080394e672d5fe6c204428b06be0d4b
chekunov <v.chekunov@media108.ru>
2023-04-06 15:58:41 +0300

    #9379222 Изменены импорты в меню.

--- tgbot/keyboards/flat_selection.py
+++ tgbot/keyboards/flat_selection.py
@@ -1,11 +1,11 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
-from tgbot.keyboards.building_menu import menu_cd
 from tgbot.keyboards.make_call import call_button
 from tgbot.keyboards.send_contact import contact_button
 
 flat_selection_cd = CallbackData('selection', 'building_name', 'option')
+menu_cd = CallbackData('menu', 'name')
 
 
 async def flat_selection_keyboard(building_name: str) -> InlineKeyboardMarkup:
