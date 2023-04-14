f7d2d0598bb507e6e9a864201b680af2cf63cd31
chekunov <v.chekunov@media108.ru>
2023-04-14 15:08:45 +0300

    #9379222 Корректно получаем название building_name.

--- tgbot/handlers/show_flats.py
+++ tgbot/handlers/show_flats.py
@@ -19,6 +19,7 @@ from tgbot.utils.page import get_page
 async def show_chosen_flats(call: CallbackQuery, state: FSMContext, callback_data: dict):
     data = await state.get_data()
     building_name = data.get('building_name') or callback_data.get('building_name')
+    print(building_name)
     ordering = 'area_low_to_high'
     rooms = callback_data.get('option')
     offers = await get_offers(building_name, rooms, ordering)
