75f5c7ce6e2a12b8f1ace61a463fe4d541ae6882
chekunov <v.chekunov@media108.ru>
2023-04-14 15:55:46 +0300

    #9379222 Исправлен просмотр Пентхаусов из главного меню.

--- tgbot/keyboards/building_menu.py
+++ tgbot/keyboards/building_menu.py
@@ -1,12 +1,12 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
+from tgbot.keyboards.flat_selection import flat_selection_cd
 from tgbot.keyboards.make_call import call_button
 from tgbot.keyboards.send_contact import contact_button
 
 building = CallbackData('building', 'name', 'section')
 menu_cd = CallbackData('menu', 'name')
-flat_selection_cd = CallbackData('selection', 'building_name', 'option')
 
 
 async def main_building_menu(building_name: str) -> InlineKeyboardMarkup:
